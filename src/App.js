b5277d782e49b64ba39f9b4b7acffa77d671a7c0
AndresBo <anndresfborrero@gmail.com>
2023-07-27 21:50:39 +1000

    build: implemented login functionality. When the login succeeds, token is saved to the app state and blogs are displayed. If user is not logged, a login form is displayed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import { useState, useEffect } from 'react'
+import LoginForm from './components/LoginForm'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [message, setMessage] = useState(null)
+
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,14 +18,46 @@ const App = () => {
     )  
   }, [])
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
-}
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch(exception) {
+      setMessage('Wrong credentials')
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
+    }
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <LoginForm
+          handleLogin={handleLogin}
+          setUsername={setUsername}
+          username={username}
+          password={password}
+          setPassword={setPassword}
+        />
+      </div>
+  )} else {
+    return (
+      <div>
+        <h2>blogs</h2>
+        <p>{user.name} is logged in</p>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    )
+  }}
 
 export default App

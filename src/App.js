09e309f494c6b38bf02f73cd4f1c802f3e1a4869
AndresBo <anndresfborrero@gmail.com>
2023-07-28 10:02:31 +1000

    build: implemented user logout functionality

--- src/App.js
+++ src/App.js
@@ -46,6 +46,13 @@ const App = () => {
     }
   }
 
+  const handleLogout = async (event) => {
+    event.preventDefault()
+
+    window.localStorage.clear()
+    setUser(null)
+  }
+
   if (user === null) {
     return (
       <div>
@@ -62,6 +69,7 @@ const App = () => {
       <div>
         <h2>blogs</h2>
         <p>{user.name} is logged in</p>
+        <button type='submit' onClick={handleLogout}>logout</button>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}

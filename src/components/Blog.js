9031f65fef8cbc5157981d8620643f5d3f33e6e2
AndresBo <anndresfborrero@gmail.com>
2023-08-01 11:51:12 +1000

    build: modified Blog component with buttons that show or hide full blog details.

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,36 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+
+  const setVisibility = () => setVisible(!visible)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  if(visible) {  
+  return (
+  <div style={blogStyle}>
+    <div>{blog.title}</div> 
+    <div>{blog.author}</div> 
+    <div>{blog.url} </div> 
+    <div>likes {blog.likes} <button>like</button></div>
+    <button onClick={setVisibility}>hide</button>
   </div>  
-)
+  ) 
+} else {
+  return (
+  <div style={blogStyle}>
+    <div>{blog.title}</div>
+    <button onClick={setVisibility}>show</button>
+  </div>
+  )
+}
+}
 
 export default Blog
